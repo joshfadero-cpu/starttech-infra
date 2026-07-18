4a95386afe167d69f0c6e628e69223932f1190ce
Joshua Fadero <joshfadero@gmail.com>
2026-07-18 12:03:53 +0100

    feat: enable eks access entries via api_and_config_map authentication mode

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -7,6 +7,11 @@ resource "aws_eks_cluster" "starttech" {
   version  = var.cluster_version
   role_arn = aws_iam_role.cluster.arn
 
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
   vpc_config {
     subnet_ids              = var.private_subnet_ids
     endpoint_public_access  = true
